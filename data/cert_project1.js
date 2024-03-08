0a7f1d7450dcbd8e0e8807de8f3b829bf4da8983
ahlamdata <mahlam37@gmail.com>
2024-03-07 21:30:20 -0400

    I added a welcome message

---
+++ data/cert_project1.js
@@ -0,0 +1,1 @@
+console.log("Welcome to Random Code Generator");
---
+++ index.js
@@ -0,0 +1,4 @@
+console.log('Starting point for Certification Project I')
+
+// You can run this file either with node index.js, or npm run start in the terminal
+// Feel free to make use of the utils folder, so extracting some code from here to js files you create there
