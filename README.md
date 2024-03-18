58dd706f4f1fed63142523244c7dc02404482ca8
ahlamdata <mahlam37@gmail.com>
2024-03-18 01:27:16 -0300

    I added a while loop and prompt that allows the user to interact with both the QuoteList and the individual quotes in them

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # certification-project1
 Practical Knowledge Demo 
+The purpose of this application is to display multiple categories of quotes in the console. Then the user decide which category they will prefer to read, this selected category of quote is then displayed in the console for them. 
+
+User can exit the QuoteManager at anytime by typing q to quit or c to continue else QuoteManager authomatically continues to display quotes.
+User has the option to add, delete or update both the array of QuoteList object and array of quotes object in the QuoteManager.
--- index.js
+++ index.js
@@ -1,51 +1,114 @@
 console.log('Starting point for Certification Project I')
 
 let quoteManager = [
-    {id:1, name:"Inspirational Quotes", quotes:[{iq:1, quote:"The only way to do great work is to love what you do.", author:"Steve Jobs", category:"inspirational"}, 
-                                                {iq:2, quote:"Believe you can and you're halfway there.", author:"Theodore Roosevelt", category:"inspirational"}
+    {id:0, name:"Inspirational Quotes", quotes:[{quote:"The only way to do great work is to love what you do.", author:"Steve Jobs", category:"inspirational"}, 
+                                                {quote:"Believe you can and you're halfway there.", author:"Theodore Roosevelt", category:"inspirational"}
     ]},
-    {id:2, name:"Health and lifestyle Quotes", quotes:[{iq:1, quote:"If you want to live a happy life, tie it to a goal, not to people or things.", 
+    {id:1, name:"Health and lifestyle Quotes", quotes:[{quote:"If you want to live a happy life, tie it to a goal, not to people or things.", 
                                                                         author:"Albert Einstein", category:"health and lifesyle"},
-                                                        {iq:2, quote:"Sufficient sleep, exercise, healthy food, friendship, and peace of mind are necessities, not luxuries.", author:"Mark Halperin", 
+                                                        {quote:"Sufficient sleep, exercise, healthy food, friendship, and peace of mind are necessities, not luxuries.", author:"Mark Halperin", 
                                                         category:"health and lifesyle"},
-                                                        {iq:3, quote:"Take care of your body. Its the only place you have to live.", author:"Jim Rohn", category:"health and lifesyle"}
+                                                        {quote:"Take care of your body. Its the only place you have to live.", author:"Jim Rohn", category:"health and lifesyle"}
 
     ]},
-    {id:3, name:"Fate Quotes", quotes:[{iq:1, quote:"Allah does not burden a soul beyond that it can bear", author:"Quran 2:286", category:"fate"},
-                                        {iq:2, quote:"And whoever relies upon Allah, then He is sufficient for him. Indeed, Allah will accomplish His purpose. Allah has already set for everything a decreed extent", 
+    {id:2, name:"Fate Quotes", quotes:[{quote:"Allah does not burden a soul beyond that it can bear", author:"Quran 2:286", category:"fate"},
+                                        {quote:"And whoever relies upon Allah, then He is sufficient for him. Indeed, Allah will accomplish His purpose. Allah has already set for everything a decreed extent", 
                                         author:"Quran 65:3", category:"fate"},
-                                        {iq:3, quote:"He who does not show mercy to others will not be shown mercy", author:"Prophet Muhammad, peace be upon him", category:"fate"}
+                                        {quote:"He who does not show mercy to others will not be shown mercy", author:"Prophet Muhammad, peace be upon him", category:"fate"}
 
     ]},
-    {id:4, name:"Influence Quotes", quotes:[{iq:1, quote:"Example is not the main thing in influencing others. It is the only thing", author:"Albert Schweitzer", category:"influence"},
-                                        {iq:2, quote:"Children are more influenced by sermons you act than by sermons you preach.", author:"David McKay", category:"influence"},
-                                        {iq:3, quote:"If you try to influence everyone, you wont influence anyone.", author:"Josh Steimle", category:"influence"}
+    {id:3, name:"Influence Quotes", quotes:[{quote:"Example is not the main thing in influencing others. It is the only thing", author:"Albert Schweitzer", category:"influence"},
+                                        {quote:"Children are more influenced by sermons you act than by sermons you preach.", author:"David McKay", category:"influence"},
+                                        {quote:"If you try to influence everyone, you wont influence anyone.", author:"Josh Steimle", category:"influence"}
     ]},
-    {id:5, name:"Tech Quotes", quotes:[{iq:1, quote:"The technology you use impresses no one. The experience you create with it is everything.", author:"Sean Gerety", category:"tech"},
-                                        {iq:2, quote:"The advance of technology is based on making it fit in so that you dont really even notice it, so its part of everyday life.", author:"Bill Gates",
+    {id:4, name:"Tech Quotes", quotes:[{quote:"The technology you use impresses no one. The experience you create with it is everything.", author:"Sean Gerety", category:"tech"},
+                                        {quote:"The advance of technology is based on making it fit in so that you dont really even notice it, so its part of everyday life.", author:"Bill Gates",
                                          category:"tech"},
-                                        {iq:3, quote:"Technology is best when it brings people together.", author:"Matt Mullenweg", category:"tech"}
-]}
-    
+                                        {quote:"Technology is best when it brings people together.", author:"Matt Mullenweg", category:"tech"}
+]}   
 ]
 
-console.log("Welcome to Quote Manager")
-console.log(quoteManager);
+
+// Declaring key variables
+let iq = Number;
+let quote = "";
+let author = ""
+let category = ""
+id = function() {
+    for (let i=0; i<quoteManager.length; i++){
+        id = quoteManager[i];
+    }; //id number, define a for loop for id and make it a function
+} 
+let name = "string"   //prefer quotelist name
+let quotes = [{quote, author,category},
+              {quote, author,category},
+              {quote, author,category}] // details of quotes(quote, author, category)
+
+// create quoteList and quotes
+quoteManager.push({
+    id, 
+    name, 
+    quotes
+});
+
+
+
+// ***deleting quoteList***
+let deleteQuoteList= function(id){
+    delete quoteManager[id]
+} 
+
+// ***deleting quotes***
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const prompt = require('prompt-sync')();
-let userInput = prompt("Enter your prefered category of quotes (inspirational, health and lifestyle, fate, influence or tech): ");
-
-if (userInput == "inspirational".toLowerCase()){
+let viewQuoteManager = true;
+while(viewQuoteManager){
+    console.log("Welcome to Quote Manager")
+    console.log(quoteManager);
+    let userInput = prompt("Enter your prefered category of quotes (inspirational, health and lifestyle, fate, influence or tech): ");
+    if (userInput == "inspirational".toLowerCase()){
     console.log(quoteManager[0]);
-}else if(userInput == "health and lifestyle".toLowerCase()){
+    }else if(userInput == "health and lifestyle".toLowerCase()){
     console.log(quoteManager[1]);
-}else if(userInput == "fate".toLowerCase()){
+    }else if(userInput == "fate".toLowerCase()){
     console.log(quoteManager[2]);
-}else if(userInput == "influence".toLowerCase()){
+    }else if(userInput == "influence".toLowerCase()){
     console.log(quoteManager[3])
+    
+    }else if (userInput == "tech".toLowerCase()){
     console.log(quoteManager[4]);
-}else if (userInput == "tech".toLowerCase()){
-    console.log(quoteManager)
+    }
+
+
+    // // Adding/editing/deleting quoteList and quotes
+    // newUserInput = prompt("Would you like to add to the quoteList, yes/no: ")
+    // name = prompt("How would you name this set of quotes in the quote list?: "); // enter the name of the quote to be added in the quotelist
+    // quote = prompt("Enter your quote here: "); //prompt for adding quote to the existing quotes
+    // author = prompt("Who said this beautiful quote (first and last names of author of the quote)?: "); //author of the quote added
+    // category = prompt("How would you classify this quote?: "); // category of quote added
+
+
+    const quitQuoteManager = prompt("Do you want to quit viewing QuoteManager or continue viewing more quotes (enter q to quit/or c to continue)?: ").toLowerCase();
+    if (quitQuoteManager === "q") {
+        console.log("Thanks for viewing and using QuoteManager. Hope you liked the quotes!!!");
+        viewQuoteManager = false;
+    }else if (quitQuoteManager !== "q" && quitQuoteManager !== "c"){
+        console.log("Please enter q or c. Thank you")
+    }
 }
 
 
@@ -57,12 +120,3 @@ if (userInput == "inspirational".toLowerCase()){
 
 
 
-
-// const fs = require("fs"); //declaring fs
-// // Write a function to reshuffle the positions of the quotes(elements) in the list.
-// // Conditional Logic #change these repetitive codes into a function or use the :
-// console.log(mainquote)
-
-
-  // You can run this file either with node index.js, or npm run start in the terminal
-// Feel free to make use of the utils folder, so extracting some code from here to js files you create there
